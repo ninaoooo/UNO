4b8d4ffc92e9cd30357bf34bb29aa303a37baaaf
ninaoooo <1584431427@qq.com>
2025-03-17 16:50:52 +0800

    修改了 PlayerInfo.lua 文件, 将PlayerInfo 中不需要的属性和方法删除

--- Assets/Lua/Tools/PlayerInfo.lua
+++ Assets/Lua/Tools/PlayerInfo.lua
@@ -1,20 +1,11 @@
 local PlayerInfo = {
     playerId = "",
     playerName = "",
-    passWord = "",
 }
 
 -- Set 方法
-function PlayerInfo:SetPlayerId(playerId)
-    self.playerId = playerId
-end
-
 function PlayerInfo:SetPlayerName(playerName)
     self.playerName = playerName
-end
-
-function PlayerInfo:SetPassword(password)
-    self.passWord = password
 end
 
 -- Get 方法
@@ -26,8 +17,9 @@ function PlayerInfo:GetPlayerName()
     return self.playerName
 end
 
-function PlayerInfo:GetPassword()
-    return self.passWord
+function PlayerInfo:ClearUser()
+    self.playerId = ""
+    self.playerName = ""
 end
 
 return PlayerInfo
