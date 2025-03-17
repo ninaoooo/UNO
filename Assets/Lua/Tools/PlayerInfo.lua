a7f0f32c0659a8fe6f880fcc1edb2f7607f9236a
ninaoooo <1584431427@qq.com>
2025-03-17 16:13:58 +0800

    修改了 PlayerInfo.lua 文件, 将PlayerInfo该为了单例模式

--- Assets/Lua/Tools/PlayerInfo.lua
+++ Assets/Lua/Tools/PlayerInfo.lua
@@ -1,14 +1,33 @@
--- 定义 PlayerInfo 类
-PlayerInfo = {
+local PlayerInfo = {
     playerId = "",
     playerName = "",
     passWord = "",
 }
 
-
-
-function PlayerInfo:ClearUser()
-    self.playerId = ""
-    self.playerName = ""
-    self.passWord = ""
+-- Set 方法
+function PlayerInfo:SetPlayerId(playerId)
+    self.playerId = playerId
 end
+
+function PlayerInfo:SetPlayerName(playerName)
+    self.playerName = playerName
+end
+
+function PlayerInfo:SetPassword(password)
+    self.passWord = password
+end
+
+-- Get 方法
+function PlayerInfo:GetPlayerId()
+    return self.playerId
+end
+
+function PlayerInfo:GetPlayerName()
+    return self.playerName
+end
+
+function PlayerInfo:GetPassword()
+    return self.passWord
+end
+
+return PlayerInfo
