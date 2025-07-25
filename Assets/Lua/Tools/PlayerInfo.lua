49c4a1537fcac76d649e10a3132861d0db2164b4
ninaoooo <233050392@hdu.edu.cn>
2025-07-25 19:15:37 +0800

    添加了背包字段

--- Assets/Lua/Tools/PlayerInfo.lua
+++ Assets/Lua/Tools/PlayerInfo.lua
@@ -6,14 +6,19 @@ local PlayerInfo = {
         ["1000001"] = { playerName = "一颗花椰菜", playerAvatar = "avatar (1)" },
         ["1000002"] = { playerName = "一瓶椰子水", playerAvatar = "avatar (2)" }
     },
+    Bag = {},
+    gold = 0,
+    diamond = 0
 }
 
+local serverBagData = {size = 48, slots = {{slot = 1, ID = 1, count = 10 },{slot = 2, ID = 2, count = 5}}}
 function PlayerInfo:InitPlayerInfo(result,playerId,playerName)
     if result then
         local idx = playerId%13
         self:SetPlayerId(playerId)
         self:SetPlayerName(playerName)
         self:SetPlayerAvatar("avatar ("..idx..")")
+        self.Bag = BagMgr:CreateBag(serverBagData.slots,serverBagData.size)
     end
 end
 MessageSystem.RegisterListener("S2C.LoginUserResult",function(result,playerId,playerName)
@@ -56,4 +61,8 @@ function PlayerInfo:IsSelf(playerId)
     return self.playerId == playerId
 end
 
+
+
+
 return PlayerInfo
+
