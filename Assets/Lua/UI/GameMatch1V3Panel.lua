9293e4d1173dfc27d13615975f7698d38800452d
ninaoooo <233050392@hdu.edu.cn>
2025-07-29 10:33:38 +0800

    4人场和同步和2人场的修改

--- Assets/Lua/UI/GameMatch1V3Panel.lua
+++ Assets/Lua/UI/GameMatch1V3Panel.lua
@@ -11,12 +11,12 @@ GameMatch1V3Panel.__index = GameMatch1V3Panel  -- 实例级元表指向自身
 -- 子类构造函数
 function GameMatch1V3Panel:New()
     -- 1. 创建实例并继承基类元表
-    local self = GameMatchBasePanel:New()
+    local instance = GameMatchBasePanel:New(self)
     -- 2. 覆盖元表为子类元表
-    setmetatable(self, GameMatch1V3Panel)
+    setmetatable(instance, self)
     -- 3. 设置子类属性
-    self.panelName = "GameMatch1V3Panel"
-    return self
+    instance.panelName = "GameMatch1V3Panel"
+    return instance
 end
 
 -- 子类必须实现的方法
