6ef70c8e86e3aec55de40b9040590e8fac9136cc
ninaoooo <233050392@hdu.edu.cn>
2025-07-30 18:20:57 +0800

    删除不必要的代码

--- Assets/Lua/UI/Pools/CardPool.lua
+++ Assets/Lua/UI/Pools/CardPool.lua
@@ -12,14 +12,13 @@ end
 
 function CardPool:clean(obj)
     print("CardPool:clean - Cleaning card object: ")
-    local rectTransform = obj:GetComponent(typeof(CS.UnityEngine.RectTransform))
-    rectTransform.localPosition = Vector3(0, 0, 0)
-    
+    local rectTransform = obj:GetComponent(typeof(RectTransform))
+
     -- 2. 清除旋转（完全重置）
     rectTransform.localRotation = CS.UnityEngine.Quaternion.identity
     
     -- 3. 清除缩放（恢复为 1,1,1）
-    rectTransform.localScale = CS.UnityEngine.Vector3(1, 1, 1)
+    rectTransform.localScale = Vector3(1, 1, 1)
     local btn = obj:GetComponent("Button")
     if btn then
         btn.onClick:RemoveAllListeners()
